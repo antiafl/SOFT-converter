d610baac6fe0eeabc779144308d244e394965aa4
Antia Fraga Lourido <antia.fraga.lourido@udc.es>
2023-04-11 09:54:34 +0200

    [MRNET-1] Modified SubsetInfo declaration + Modified code to add row with class values to data + Added code to print progress while processing soft file

--- utils/soft-file-reader/processSoftFile.py
+++ utils/soft-file-reader/processSoftFile.py
@@ -1,20 +1,29 @@
 import sys
 import argparse
 import GEOparse
-from pprint import pprint
+from time import sleep
 import numpy as np
 import pandas as pd
 
 class SubsetInfo:
     # The init method or constructor
-    def __init__(self, type, values, option):
+    def __init__(self, subsets_list, type, descriptions_list, option):
         # Instance Variable
+        self.subsets_list = subsets_list
         self.type = type
-        self.values = values
+        self.descriptions_list = descriptions_list
         self.option = option
 
     def __repr__(self):
         return str(self)
+
+    # Adds an instance variable
+    def setsubsets_list(self, subsets_list):
+        self.subsets_list = subsets_list
+
+    # Retrieves instance variable
+    def getsubsets_list(self):
+        return self.subsets_list
 
     # Adds an instance variable
     def settype(self, type):
@@ -25,12 +34,12 @@ class SubsetInfo:
         return self.type
 
     # Adds an instance variable
-    def setvalues(self, values):
-        self.values = values
+    def setdescriptions_list(self, descriptions_list):
+        self.descriptions_list = descriptions_list
 
     # Retrieves instance variable
-    def getvalues(self):
-        return self.values
+    def getdescriptions_list(self):
+        return self.descriptions_list
 
     # Adds an instance variable
     def setoption(self, option):
@@ -40,10 +49,7 @@ class SubsetInfo:
     def getoption(self):
         return self.option
 
-# def valsNotNumeric(vals):
-#     return type(vals.values[0]) == np.float32 or type(vals.values[0]) == np.float64
-
-# def create_CSV_from_SOFT(fff):
+# def export_df_to_CSV(df):
 #     gds = GEOparse.get_GEO(filepath=fff)
 #     df = gds.table
 #     dfCols = df.columns
@@ -64,53 +70,108 @@ def get_subsetInfo_by_type(type, subsetInfo_list):
             obj_index_in_list = index
     return obj_in_list, obj_index_in_list
 
-def get_types_with_descriptions_from_data(fff):
-    geo_file = GEOparse.get_GEO(filepath=fff, silent=True)
-    geo_subsets = geo_file.subsets
+def add_to_list(item, list):
+    list.append(item)
+    return list
+
+def create_SubsetInfo_objs_from_subsets(geo_subsets):
     subset_type_list = []
     subsetInfo_obj_list = []
     subsetInfo_option_value = 0
-    for gs in geo_subsets:
-        subset = geo_subsets.get(gs)
+    for gs_name in geo_subsets:
+        subset = geo_subsets.get(gs_name)
         gs_type = subset.metadata['type'][0]
         gs_description = subset.metadata['description'][0]
         if (gs_type not in subset_type_list):
             # add new subset type to list
             subset_type_list.append(gs_type)
-            # create & add new SubsetInfo object with new type & description
-            New_SubsetInfo_obj = SubsetInfo(gs_type, [gs_description], subsetInfo_option_value)
+            # create & add new SubsetInfo object with name, type, description & option
+            New_SubsetInfo_obj = SubsetInfo([gs_name], gs_type, [gs_description], subsetInfo_option_value)
             subsetInfo_option_value += 1
             subsetInfo_obj_list.append(New_SubsetInfo_obj)
         else:
             # get object by type, get existing descriptions, add new description & modify in list
             SubsetInfo_obj, subsetInfo_index = get_subsetInfo_by_type(gs_type, subsetInfo_obj_list)
-            subsetInfo_obj_values_list = SubsetInfo_obj.getvalues()
-            subsetInfo_obj_values_list.append(gs_description)
-            SubsetInfo_obj.setvalues(subsetInfo_obj_values_list)
+            # subsetInfo_obj_descriptions_list = SubsetInfo_obj.getdescriptions_list()
+            # subsetInfo_obj_descriptions_list.append(gs_description)
+            subsetInfo_subsets_list = add_to_list(gs_name, SubsetInfo_obj.getsubsets_list())
+            SubsetInfo_obj.setsubsets_list(subsetInfo_subsets_list)
+            subsetInfo_descriptions_list = add_to_list(gs_description, SubsetInfo_obj.getdescriptions_list())
+            SubsetInfo_obj.setdescriptions_list(subsetInfo_descriptions_list)
             subsetInfo_obj_list[subsetInfo_index] = SubsetInfo_obj
-    subsetInfo_options_list = map(lambda x: x.option, subsetInfo_obj_list)
-    return subsetInfo_obj_list, subsetInfo_options_list
+    return subsetInfo_obj_list
 
-def prompt_info_to_user(input_file, subsetInfo_list, optionsList):
+def prompt_info_to_select_option(input_file, subsetInfo_list, options_list):
     print('-'*200+'\n')
     print("\nThe subsets in input file ("+input_file+") contain the following types and descriptions.\n")
-    for o in options_list:
-        print(type(o))
-        print(o)
     for si in subsetInfo_list:
         print(f"\t Option {si.option}: "+si.type)
-        print(f"\t\t values: {si.values}\n")
+        print(f"\t\t descriptions: {si.descriptions_list}\n")
+        print(f"\t\t subsets: {si.subsets_list}\n")
     selected_option = -1
-    while (selected_option not in optionsList):
-        input_option = input("\nSelect a subset type in order to create the input data for Parallel-MRNET:")
+    while (selected_option not in options_list):
+        input_option = input(f"\nSelect an option from the following {options_list}: \n")
         try:
             selected_option = int(input_option)
         except:
-            print("Option must be a number")
-        print(selected_option in options_list)
-        print(type(selected_option))
-        print(selected_option)
+            print(f"**ERROR: Option must be one of the following numbers {options_list}**")
+    print('-'*200+'\n')
+    return selected_option
 
+def get_columnList_classList_from_subsets_samples_names(geo_subsets, subsetInfo_list, option, index):
+    subset_name_list = subsetInfo_list[option].getsubsets_list()
+    # index = 'ID_REF'
+    column_values_list = [index]
+    class_value = 0
+    class_values_list = []
+    for gs_name in subset_name_list:
+        # get column names from all subsets that correspond with the option selected by the user
+        subset = geo_subsets.get(gs_name)
+        gs_samples = subset.metadata['sample_id'][0].split(',')
+        column_values_list += gs_samples
+        row_subset_samples = [class_value] * len(gs_samples)
+        class_values_list +=  (row_subset_samples)
+        class_value += 1
+    return column_values_list, class_values_list
+
+def create_df_from_geo_df(index_values, df, class_values):
+    # index: id array (first column of geo df)
+    # df: data
+    # dictionary:
+    #   {
+    #       GeneId_1: [values for geneId_1 contained in row 1],
+    #       ...,
+    #       GeneId_N: [values for geneId_N contained in row N],
+    #       Class: [class values]
+    #   }
+    dictionary = {}
+    n = len(index_values)
+    for i in range(0, n):
+        key = index_values[i]
+        row = df.iloc[i]
+        dictionary[key] = row.values
+        j = i + 1
+        # print progress processing rows
+        if (j) % (600) == 0:
+            sys.stdout.write("\rProcessed %d rows out of %d" % (j, n))
+            sys.stdout.write("\r")
+            sys.stdout.flush()
+            sleep(0.25)
+    print(f"All {n} rows processed\n")
+    dictionary["Class"] = class_values
+    print("\nNew dataframe:")
+    return pd.DataFrame.from_dict(dictionary)
+
+def get_table_data(geo_file, subsetInfo_list, option):
+    # df: dataframe with all original data
+    df = geo_file.table
+    # get subset samples names (actual columns) and class values for each sample using the option selected by the user in order to get only the needed data in next step
+    my_columns, row_class = get_columnList_classList_from_subsets_samples_names(geo_file.subsets, subsetInfo_list, option, index=df.columns[0])
+    # df_index: first column that contains all unique ids that will be used as column names
+    df_index = df[my_columns[0]].tolist()
+    df_data = df[my_columns[1: ]]
+    new_df = create_df_from_geo_df(index_values=df_index, df=df_data, class_values=row_class)
+    return new_df
 
 if __name__ == '__main__':
     # PARSE INPUT ARGS
@@ -125,14 +186,19 @@ if __name__ == '__main__':
         print("Use --help for more info")
         sys.exit()
 
-    # GET DATA
-    subsetInfo_list, options_list = get_types_with_descriptions_from_data(args.file)
-    # GET CLASS
-    prompt_info_to_user(args.file, subsetInfo_list, options_list)
+    # GET GEO FILE & SUBSETS
+    geo_file = GEOparse.get_GEO(filepath=args.file, silent=True)
+    # GET TYPES WITH DESCRIPTIONS, SUBSETS & OPTIONS
+    subsetInfo_list = create_SubsetInfo_objs_from_subsets(geo_file.subsets)
+    types_options_list = list(map(lambda x: x.option, subsetInfo_list))
+    # GET TYPE TO USE FOR CLASS
+    user_selected_option = prompt_info_to_select_option(args.file, subsetInfo_list, types_options_list)
+    # GET DATA USING SELECTED OPTION
+    data = get_table_data(geo_file, subsetInfo_list, user_selected_option)
+    print(data)
 
-    # gds3244_subsetInfo_list = get_types_with_descriptions_from_data("/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS3244_full.soft.gz")
-    # gds3795_subsetInfo_list = get_types_with_descriptions_from_data("/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS3795_full.soft.gz")
-    # gds5037_subsetInfo_list = get_types_with_descriptions_from_data("/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS5037_full.soft.gz")
+    # export_df_to_CSV("/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS3795_full.soft.gz")
 
-    # prompt_info_to_user(gds3244_subsetInfo_list)
-    # create_CSV_from_SOFT("/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS3795_full.soft.gz")
+    # "/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS3244_full.soft.gz"
+    # "/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS3795_full.soft.gz"
+    # "/home/antinux/TFM/Parallel-MRNET/parallel-MRNET/examples/GDS5037_full.soft.gz"
